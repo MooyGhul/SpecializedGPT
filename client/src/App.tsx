15c8bcb47ca374219f635eb154e349dd4dbf08b8
unknown <feelingzty@gmail.com>
2024-09-11 21:13:55 -0700

    clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState,useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/Registration';
 import GptsListPage from './pages/GptsListPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import GptDetailPage from './pages/GptDetailPage';
@@ -13,39 +11,13 @@ type Item = {
 
 function App() {
   const [items, setItems ] = useState<Item[]>([]);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
-    // Initialize state from localStorage
-    return localStorage.getItem('isLoggedIn') === 'true';
-  });
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      fetch("http://localhost:8080/api/items")
-        .then(res => res.json())
-        .then(data => setItems(data));
-    }
-  }, [isLoggedIn]);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    localStorage.setItem('isLoggedIn', 'true');
-  };
 
   return (
     <div className="App">
       <Routes>
-        
-        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
-        <Route path="/registration" element={<RegistrationPage />} />
-
-       
-        <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
-          <Route path="/homepage" element={<GptsListPage items={items} />} />
-          <Route path="/homepage/:gptId" element={<GptDetailPage />} />
-        </Route>
-
-        
-        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/homepage" element={<GptsListPage items={items} />} />
+        <Route path="/homepage/:gptId" element={<GptDetailPage />} />
+        <Route path="/" element={<Navigate to="/homepage"/>} />
       </Routes>
     </div>
   );
